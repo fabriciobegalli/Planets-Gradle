aeffb528431295840b20ce94401f3ff09e939db5
Andrew Quebe <andrewquebe@amqtech.com>
2015-06-18 11:47:09 -0700

    Remove in-app billing setup
    temporary to fix emulator issues.

--- App/src/main/java/com/andrewq/planets/NavDrawerActivity.java
+++ App/src/main/java/com/andrewq/planets/NavDrawerActivity.java
@@ -80,7 +80,7 @@ public class NavDrawerActivity extends ActionBarActivity {
 
         //TODO: Fix this!
 
-        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+        /*mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
             public void onIabSetupFinished(IabResult result) {
                 Log.d(TAG, "Setup finished.");
 
@@ -97,7 +97,7 @@ public class NavDrawerActivity extends ActionBarActivity {
                 Log.d(TAG, "Setup successful. Querying inventory.");
                 mHelper.queryInventoryAsync(mGotInventoryListener);
             }
-        });
+        });*/
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
